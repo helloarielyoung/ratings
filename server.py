992d42ae2f282eb1647c2695e25a30a9675e62f0
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-02 17:51:15 -0700

    Handles login and logout

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ from jinja2 import StrictUndefined
 
 from flask import jsonify
 from flask import (Flask, render_template, redirect, request, flash,
-                   session)
+                   session, url_for)
 from flask_debugtoolbar import DebugToolbarExtension
 
 from model import User, Rating, Movie, connect_to_db, db
@@ -28,6 +28,44 @@ def index():
     return render_template("homepage.html")
 
 
+@app.route('/login')
+def login():
+    """Display login form"""
+
+    return render_template("login_form.html")
+
+
+@app.route('/login-completion', methods=["POST"])
+def authenticate_login():
+    """authenticates user info and logs in"""
+
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if User.query.filter_by(email=email).first() is not None:
+        user = User.query.filter_by(email=email).first()
+        if user.password == password:
+            session['login'] = user.user_id
+            print session
+            flash('You were successfully logged in')
+            return redirect(url_for('index'))
+    else:
+        flash('Bad password or user name')
+
+        return redirect(url_for('login'))
+
+
+@app.route('/logout')
+def logout():
+    """logs out user"""
+
+    del session['login']
+    print session
+    flash("You are logged out")
+
+    return redirect('/')
+
+
 @app.route("/users")
 def user_list():
     """Show list of users."""
@@ -38,11 +76,15 @@ def user_list():
 
 @app.route('/register', methods=["GET"])
 def give_form():
+    """Display form to register new user"""
+
     return render_template("register_form.html")
 
 
 @app.route('/register', methods=["POST"])
 def register_process():
+    """Create new user login from registration form"""
+
     email = request.form.get("email")
     password = request.form.get("password")
     age = request.form.get("age")
@@ -50,7 +92,8 @@ def register_process():
 
     # check if email exists, and if not add to DB
     if User.query.filter_by(email=email).first() is not None:
-        return redirect("/")
+        flash("You already have an account!")
+
     else:
         user = User(email=email,
                     password=password,
@@ -58,6 +101,7 @@ def register_process():
                     zipcode=zipcode)
         db.session.add(user)
         db.session.commit()
+        flash("You are registered!")
 
     return redirect("/")
 
@@ -65,7 +109,7 @@ def register_process():
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
-    app.debug = False
+    app.debug = True
     app.jinja_env.auto_reload = app.debug  # make sure templates, etc. are not cached in debug mode
 
     connect_to_db(app)
