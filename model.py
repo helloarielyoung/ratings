2571bb13cb75fdeeeb99cbd58f953a1a8fe9e44e
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-04 12:58:17 -0700

    created similarity method in class Users

--- model.py
+++ model.py
@@ -29,6 +29,26 @@ class User(db.Model):
 
         return "<User user_id=%s email=%s>" % (self.user_id, self.email)
 
+    def similarity(self, other):
+        """Return Pearson rating for user compared to other user."""
+
+        u_ratings = {}
+        paired_ratings = []
+
+        for r in self.ratings:
+            u_ratings[r.movie_id] = r
+
+        for r in other.ratings:
+            u_r = u_ratings.get(r.movie_id)
+            if u_r:
+                paired_ratings.append((u_r.score, r.score))
+
+        if paired_ratings:
+            return pearson(paired_ratings)
+
+        else:
+            return 0.0
+
 
 class Movie(db.Model):
     """Table of movie information."""
